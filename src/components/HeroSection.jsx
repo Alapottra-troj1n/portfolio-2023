ec50231bd7712a206a185c04f6618d79f1598123
Alapottra <alapottratrojon@gmail.com>
2023-02-12 21:46:39 +0600

    added 2 more projects

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,42 +1,70 @@
-import React from 'react';
-import { BsLinkedin, BsGithub, BsFacebook, BsChevronDoubleDown } from "react-icons/bs";
-import cursorChange from '../utils/cursorChange';
+import React from "react";
+import {
+  BsLinkedin,
+  BsGithub,
+  BsFacebook,
+  BsChevronDoubleDown,
+} from "react-icons/bs";
+import cursorChange from "../utils/cursorChange";
 
-const HeroSection = ({SkillSectionRef}) => {
+const HeroSection = ({ SkillSectionRef }) => {
+  const scrolltoSecond = () => {
+    SkillSectionRef.current.scrollIntoView({ behavior: "smooth" });
+  };
 
-    const scrolltoSecond = () => {
-        SkillSectionRef.current.scrollIntoView({ behavior: 'smooth' });
-      };
-
-    return (
-        <div onMouseEnter={() => cursorChange("40, 41, 31")} className="bg-secondary pt-[5rem] lg:pt-0">
-            <div className="lg:h-screen flex flex-col  justify-center container mx-auto px-5" >
-                <div className=" grid lg:grid-cols-2 grid-cols-1 " >
-                    <div className="flex flex-col justify-center order-2 lg:order-1" >
-                        <h1 className="font-display mt-5 lg:mt-0 text-7xl lg:text-9xl tracking-tight" >Hello, I'm Ala</h1>
-                        <p className="text-sm lg:text-lg text-gray-600 font-medium tracking-wide lg:w-[70%] lg:leading-8 leading-7" >I am a full-stack developer with 2+ years of experience in creating valuable solutions for clients. I have a blend of freelance and professional experience, and I am dedicated to delivering high-quality results. I take pride in understanding my client's needs and translating them into effective and impactful products. </p>
-                    </div>
-                    <div className="flex justify-end order-1 lg:order-2" >
-                        <img src="/alapottra.jpg" className="lg:w-[35rem]" alt="alapottra" />
-                    </div>
-                </div>
-                <div className='py-10 lg:py-0' >
-                    <div className="flex gap-4" >
-                        <a href=""><BsLinkedin className="lg:text-2xl text-3xl hover:text-orange" /></a>
-                        <a href=""><BsGithub className="lg:text-2xl text-3xl hover:text-orange" /></a>
-                        <a href=""><BsFacebook className="lg:text-2xl text-3xl hover:text-orange" /></a>
-
-                    </div>
-                    <div className="mt-5 text-2xl click text-orange hover:text-dark flex">
-                        <button onClick={scrolltoSecond} className="underline font-black transition-all hover:font-medium" >CHECK MY SKILLSET</button>
-                        <BsChevronDoubleDown className="mt-[5px] ml-2" />
-                    </div>
-                </div>
-            </div>
-          
+  return (
+    <div
+      onMouseEnter={() => cursorChange("40, 41, 31")}
+      className="bg-secondary pt-[5rem] lg:pt-0"
+    >
+      <div className="lg:h-screen flex flex-col  justify-center container mx-auto px-5">
+        <div className=" grid lg:grid-cols-2 grid-cols-1 ">
+          <div className="flex flex-col justify-center order-2 lg:order-1">
+            <h1 className="font-display mt-5 lg:mt-0 text-7xl lg:text-9xl tracking-tight">
+              Hello, I'm Ala
+            </h1>
+            <p className="text-sm lg:text-lg text-gray-600 font-medium tracking-wide lg:w-[70%] lg:leading-8 leading-7">
+              I am a full-stack developer with 2+ years of experience in
+              creating valuable solutions for clients. I have a blend of
+              freelance and professional experience, and I am dedicated to
+              delivering high-quality results. I take pride in understanding my
+              client's needs and translating them into effective and impactful
+              products.{" "}
+            </p>
+          </div>
+          <div className="flex justify-end order-1 lg:order-2">
+            <img
+              src="/alapottra.jpg"
+              className="lg:w-[35rem]"
+              alt="alapottra"
+            />
+          </div>
         </div>
-        
-    );
+        <div className="py-10 lg:py-0">
+          <div className="flex gap-4">
+            <a href="">
+              <BsLinkedin className="lg:text-2xl text-3xl hover:text-orange" />
+            </a>
+            <a href="">
+              <BsGithub className="lg:text-2xl text-3xl hover:text-orange" />
+            </a>
+            <a href="">
+              <BsFacebook className="lg:text-2xl text-3xl hover:text-orange" />
+            </a>
+          </div>
+          <div className="mt-5 text-2xl click  hover:text-dark flex">
+            <button
+              onClick={scrolltoSecond}
+              className="underline font-black transition-all hover:font-medium"
+            >
+              CHECK MY SKILLSET
+            </button>
+            <BsChevronDoubleDown className="mt-[5px] ml-2" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HeroSection;
--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -3,23 +3,70 @@ import { BsArrowUpRight } from "react-icons/bs";
 
 const ProjectsSection = () => {
   return (
-    <div className=" bg-dark border-t border-gray-500 px-40 text-white">
+    <div className=" bg-dark border-t border-gray-500 px-40 pb-20 text-white">
       <div className="pt-20">
         <h2 className="text-primary font-display lg:text-8xl  hover:font-light tracking-tight">
           RECENT PROJECTS
         </h2>
 
-        <div className="flex flex-col gap-10 py-10">
+        <div className="flex flex-col gap-10 mt-10 hover:bg-white hover:text-dark transition">
           <div className="border p-20">
             <div className="flex flex-col items-center">
               <h2 className="font-display text-[10rem] ">
                 HILL VALLEY PRODUCTION
               </h2>
-              <h2 className="uppercase tracking-widest">
+              <h2 className="uppercase tracking-widest font-semibold pb-5">
                 Fullstack website for a renowned photography agency from
                 Chittagong Hill Tracts
               </h2>
               <img src="/camera.svg" alt="paint3d" className="w-[15rem]" />
+
+              <div className="flex gap-3">
+                <div className="flex items-center gap-3">
+                  <p className="py-10 text-2xl">LIVE LINK</p>
+                  <BsArrowUpRight className="text-2xl" />
+                </div>
+                <div className="flex items-center gap-3">
+                  <p className="py-10 text-2xl">GITHUB</p>
+                  <BsArrowUpRight className="text-2xl" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-10 mt-10  hover:bg-orange  hover:text-white transition">
+          <div className="hover:border-orange border p-20">
+            <div className="flex flex-col items-center">
+              <h2 className="font-display text-[10rem] ">GEEK HIVE</h2>
+              <h2 className="uppercase tracking-widest font-semibold pb-5">
+                Fullstack one-stop tech blog, bringing you the freshest geeky
+                news and ideas with a friendly touch.
+              </h2>
+              <img src="/pc.svg" alt="paint3d" className="w-[15rem]" />
+
+              <div className="flex gap-3">
+                <div className="flex items-center gap-3">
+                  <p className="py-10 text-2xl">LIVE LINK</p>
+                  <BsArrowUpRight className="text-2xl" />
+                </div>
+                <div className="flex items-center gap-3">
+                  <p className="py-10 text-2xl">GITHUB</p>
+                  <BsArrowUpRight className="text-2xl" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-10 mt-10  hover:bg-primary  hover:text-dark transition">
+          <div className="hover:border-primary border p-20">
+            <div className="flex flex-col items-center">
+              <h2 className="font-display text-[10rem] ">IGNITION</h2>
+              <h2 className="uppercase tracking-widest font-semibold pb-5">
+                A FULLSTACK INVENTORY MANAGEMENT WEBSITE for tools and hardware WITH ADMIN DASHBOARD & PAYMENT GATEWAY
+              </h2>
+              <img src="/tools.svg" alt="paint3d" className="w-[15rem]" />
 
               <div className="flex gap-3">
                 <div className="flex items-center gap-3">
